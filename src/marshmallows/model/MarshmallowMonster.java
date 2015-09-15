abb424f3bc8e5aff6321daa2ef82bffeca6fb316
Adam Kinzer <adamkinzer915@gmail.com>
2015-09-15 13:01:09 -0600

    Added getters and setters

--- src/marshmallows/model/MarshmallowMonster.java
+++ src/marshmallows/model/MarshmallowMonster.java
@@ -24,6 +24,67 @@ public class MarshmallowMonster
 		this.monsterBelly = monsterBelly;
 	}
 	
+	public String getMonsterName()
+	{
+		return monsterName;
+	}
+	
+	public int getMonsterEyes()
+	{
+		return monsterEyes;
+	}
+	
+	public int getMonsterArms()
+	{
+		return monsterArms;
+	}
+	
+	public double getMonserLegs()
+	{
+		return monsterLegs;
+	}
+	
+	public double getMonsterHair()
+	{
+		return monsterHair;
+	}
+	
+	public boolean getMonsterBelly()
+	{
+		return monsterBelly;
+	}
+	
+	public void setMonsterName(String monsterName)
+	{
+		this.monsterName = monsterName;
+	}
+	
+	public void setMonsterEyes(int monsterEyes)
+	{
+		this.monsterEyes = monsterEyes;
+	}
+	
+	public void setMonsterArms(int monsterArms)
+	{
+		this.monsterArms = monsterArms;
+	}
+	
+	public void setMonsterLegs(double monsterLegs)
+	{
+		this.monsterLegs = monsterLegs;
+	}
+	
+	public void setMonsterHair(double monsterHair)
+	{
+		this.monsterHair = monsterHair;
+	}
+	
+	public void setMonsterBelly(boolean monsterBelly)
+	{
+		this.monsterBelly = monsterBelly;
+	}
+	
+	
 	public String toString()
 	{
 		String Gut;
@@ -39,7 +100,7 @@ public class MarshmallowMonster
 		String monsterInfo = "My monster has " + monsterEyes 
 							+ " eyes and its' name is " + monsterName 
 							+ " It has " + monsterArms + " arms and has " 
-							+ monsterLegs + " legs he is a little sparses but has " 
+							+ monsterLegs + " legs he is a a	little sparses but has " 
 							+ monsterHair + " hairds, does he have a belly? " + Gut;
 		
 		return monsterInfo;
